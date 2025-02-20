8bfc2b3650ea46c11b7b59983380112d2d2553da
Favour chuks <84434963+Favour-chuks@users.noreply.github.com>
2025-02-20 04:35:28 +0000

    done with the grid styles

--- frontend/src/assets/components/colorGrid.tsx
+++ frontend/src/assets/components/colorGrid.tsx
@@ -1,15 +1,14 @@
+import './styles/colorgrid.css'
+
 const ColorGrid = () => {
   return (
-    <div className="p-4 bg-white rounded-lg shadow-md max-w-sm mx-auto">
-      <h2 className="text-xl font-semibold text-center">Color Grid</h2>
-      <div className="grid grid-cols-5 gap-2 mt-4">
-        {Array.from({ length: 25 }).map((_, index) => (
-          <div
-            key={index}
-            className="w-12 h-12 rounded-full bg-gray-200"></div>
-        ))}
-      </div>
-      <div className="flex justify-center items-center mt-4"></div>
+    <div className="gridContainer">
+      <div className="jp gridItems"></div>
+      <div className="jp gridItems"></div>
+      <div className="jp gridItems"></div>
+      <div className="jp gridItems"></div>
+      <div className="jp gridItems"></div>
+      <div className="jp gridItems"></div>
     </div>
   );
 };
